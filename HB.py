6763c4b8e894c23ddbe726325e4903741f0585c1
dkppg2 <117648465+dkppg2@users.noreply.github.com>
2023-08-05 08:12:15 +0530

    Update HB.py

--- HB.py
+++ HB.py
@@ -3,8 +3,13 @@ import time
 import math
 from pyrogram import Client, filters
 import yt_dlp as yt
+from yt_dlp.utils import DownloadError
 #from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import os
+import asyncio
+import threading
+from pyrogram.errors import MessageNotModified, FloodWait
+
 
 # Telegram Bot Token
 API_ID = 14604313
@@ -18,7 +23,7 @@ app = Client("YouDl", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 url_pattern = r'(https?://[^\s]+)'
 
 # ...
-
+'''
 def progress_for_pyrogram(current, total, bot, ud_type, message, start):
     now = time.time()
     diff = now - start
@@ -83,19 +88,52 @@ def TimeFormatter(milliseconds: int) -> str:
           ((str(minutes) + "m, ") if minutes else "") + \
           ((str(seconds) + "s, ") if seconds else "")
     return tmp[:-2]
+'''
+def humanbytes(size):
+    if not size:
+        return ""
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
+
+def edit_msg(client, message, to_edit):
+    try:
+        client.loop.create_task(message.edit(to_edit))
+    except FloodWait as e:
+        client.loop.create_task(asyncio.sleep(e.value))
+    except MessageNotModified:
+        pass
+    except TypeError:
+        pass
+
+
+def download_progress_hook(d, message, client):
+    if d['status'] == 'downloading':
+        current = d.get("_downloaded_bytes_str") or humanbytes(int(d.get("downloaded_bytes", 1)))
+        total = d.get("_total_bytes_str") or d.get("_total_bytes_estimate_str")
+        file_name = d.get("filename")
+        eta = d.get('_eta_str', "N/A")
+        percent = d.get("_percent_str", "N/A")
+        speed = d.get("_speed_str", "N/A")
+        to_edit = f"📥 <b>Downloading!</b>\n\n<b>Name :</b> <code>{file_name}</code>\n<b>Size :</b> <code>{total}</code>\n<b>Speed :</b> <code>{speed}</code>\n<b>ETA :</b> <code>{eta}</code>\n\n<b>Percentage: </b> <code>{current}</code> from <code>{total} (__{percent}__)</code>"
+        threading.Thread(target=edit_msg, args=(client, message, to_edit)).start()
 #...
-def download_video(url, message):
+async def run_async(func, *args, **kwargs):
+    loop = asyncio.get_running_loop()
+    return await loop.run_in_executor(None, func, *args, **kwargs)
+
+def download_video(c, m):
     ydl_opts = {
-        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
-        'outtmpl': '%(title)s.%(ext)s',
-        'progress_hooks': [lambda d: progress_for_pyrogram(
-            d.get('downloaded_bytes', 0),
-            d.get('total_bytes', 0),
-            None,
-            'Downloading',
-            message,
-            time.time()
-        )]
+        "format": 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+       # outtmpl: '%(title)s.%(ext)s',
+       # 'progress_hooks': [lambda d: progress_for_pyrogram(
+        "progress_hooks": [lambda d: download_progress_hook(d, m, c)   
+        
     }
     with yt.YoutubeDL(ydl_opts) as ydl:
        # info_dict = ydl.extract_info(url, download=False)
