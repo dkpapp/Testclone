9ca1d00c3db381a2b0108824823fb9ee1237a5f4
dkpapp <118980727+dkpapp@users.noreply.github.com>
2024-01-02 18:17:14 +0530

    Update HB.py

--- HB.py
+++ HB.py
@@ -119,10 +119,11 @@ async def start(client, message):
 
 async def main():
     
-    await app.start()
+    #await app.start()
     logit("Hello Master Dhruv 🥳")
     await idle()
     await asyncio.gather(*[bot.run() for bot in bots])
     
 if __name__ == "__main__":
     asyncio.run(main())
+    app.run()
